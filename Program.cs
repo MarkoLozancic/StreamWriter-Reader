b27daad7722ac36e85d35b512a1000a5df277043
Korisnik <Korisnik@DESKTOP-DIMB8PE>
2022-04-05 13:08:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamPisac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upisite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            sw.WriteLine("Ime: {0}", ime);
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+
+
+            StreamReader sr = new StreamReader(@"c:\prviDir\mojaDatoteka.txt");
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            Console.ReadKey();
+        }
+    }
+}
